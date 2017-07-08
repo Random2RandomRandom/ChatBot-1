73d72dbe38562a0d0ac4a06c1fb8d05c004ea549
Random2RandomRandom <whynewacc123@gmail.com>
2017-07-08 19:32:44 +0200

    Update dealership.js

--- commands/Random/dealership.js
+++ commands/Random/dealership.js
@@ -28,14 +28,35 @@ class DealershipCommand extends commando.Command {
         else {
             message.channel.send("\n" + DataDealership[0].model + " Price: $" + DataDealership[0].price, { file: DataDealership[0].img });
             /*message.channel.send("\n" + DataDealership[1].model + " Price: $" + DataDealership[1].price, { file: DataDealership[1].img });*/
-            const embed = new Discord.RichEmbed() .setTitle("Test BOT // LETS GOO") .setColor();
-            var str = "";
-            DataDealership.forEach(function(e){
-              str += "\n"+e.model + " Price: $" + e.price;
-              str += "\n" + e.img;
-            }); embed.setDescription(str);
-            message.channel.send({embed});
-            console.log(str);console.log(embed);
+           const embed = new Discord.RichEmbed()
+  .setTitle("This is your title, it can hold 256 characters")
+  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x00AE86)
+  .setDescription("This is the main body of text, it can hold 2048 characters.")
+  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+  .setImage("http://i.imgur.com/yVpymuV.png")
+  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  /*
+   * Takes a Date object, defaults to current date.
+   */
+  .setTimestamp()
+  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+  .addField("This is a field title, it can hold 256 characters",
+    "This is a field value, it can hold 2048 characters.")
+  /*
+   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+   */
+  .addField("Inline Field", "They can also be inline.", true)
+  /*
+   * Blank field, useful to create some space.
+   */
+  .addBlankField(true)
+  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+  message.channel.send({embed});
         }
     }
 }
