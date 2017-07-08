7d4f3c53125258794b38ba4acec2e685c22e9a50
Random2RandomRandom <whynewacc123@gmail.com>
2017-07-08 16:40:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
 
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
 bot.login(process.env.BOT_TOKEN);
 
