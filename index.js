18a1b2818fbaa17c78d4ab5f3019c3ad7288d1c5
Random2RandomRandom <whynewacc123@gmail.com>
2017-07-09 18:30:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const commando = require('discord.js-commando');
+const commando = require('discord.js-commando');
 const bot = new commando.Client();
 const fs = require("fs");
 bot.registry.registerGroup('random','Random');
@@ -8,7 +8,7 @@ bot.registry.registerCommandsIn(__dirname + "/commands");
 
 
 var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
-bot.login(process.env.BOT_TOKEN);*/
+bot.login(process.env.BOT_TOKEN);/*
 
 const Discord = require('discord.js-commando');
 const client = new Discord.Client();
