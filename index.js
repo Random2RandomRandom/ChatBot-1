2ea48ce9b13af22fc83399cab79b8d8495040f6a
Random2RandomRandom <whynewacc123@gmail.com>
2017-07-09 18:55:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.registry.registerCommandsIn(__dirname + "/commands");
 
 var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.login(process.env.BOT_TOKEN);*/
-const Discord = require('discord.js-commando');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 
