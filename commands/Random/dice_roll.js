a6a4675b796fad913139bd30123688bcf2277988
Random2RandomRandom <whynewacc123@gmail.com>
2017-09-06 21:02:50 +0200

    Update dice_roll.js

--- commands/Random/dice_roll.js
+++ commands/Random/dice_roll.js
@@ -1,12 +1,27 @@
 const commando = require('discord.js-commando');
 
-class DiceRollCommand extends commando.Command {
+class addInsuranceRoleCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'roll',
-            group: 'random',
-            memberName: 'roll',
-            description: 'Rolls a die'
+            name: 'addinsurance',
+            group: 'group_insurance',
+            memberName: 'insurance',
+			description: 'Add insurance to user!.Default is 7 days.',
+			examples: ['addinsurance @Crawl#3208 7', 'addinsurance Crawl'],
+			args: [
+				{
+					key: 'member',
+					label: 'user',
+					prompt: 'What user would you like to add insurance to?',
+					type: 'member'
+				},
+                {
+                  key: 'days',
+                  prompt: 'how many days should it last?',
+                  type: 'integer'
+                  //default: 7
+                }
+			]
         });
     }
     async run(message, args) {
