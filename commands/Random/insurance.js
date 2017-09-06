52857927737f80209aa52787932632c767b5dc42
Random2RandomRandom <whynewacc123@gmail.com>
2017-09-06 22:29:16 +0200

    Update insurance.js

--- commands/Random/insurance.js
+++ commands/Random/insurance.js
@@ -1,12 +1,13 @@
 const commando = require('discord.js-commando');
-class addInsuranceRoleCommand extends commando.Command {
+
+class addTempRoleCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'addinsurance',
+            name: 'addtemprole',
 	    guildOnly: true,
             group: 'random',
-            memberName: 'insurance',
-			description: 'Add insurance to user!.Default is 7 days.',
+            memberName: 'temprole',
+			description: 'Add temporary role to user!.Default is 7 days.',
 			examples: ['addinsurance @Crawl#3208 7', 'addinsurance Crawl'],
 			args: [
 				{
@@ -15,25 +16,54 @@ class addInsuranceRoleCommand extends commando.Command {
 					prompt: 'What user would you like to add insurance to?',
 					type: 'member'
 				},
-                {
-                  key: 'days',
-                  prompt: 'how many days should it last?',
-                  type: 'integer'
-                  //default: 7
-                }
-	   ],		
+				{
+				  key: 'role',
+				  label: 'role',
+				  prompt: 'the role to temporary add',
+				  type: 'role'
+				  //default: 7
+				}
+	   			,{
+				  key: 'days',
+				  prompt: 'how many days should it last?',
+				  type: 'integer'
+				  //default: 7
+				}
+			],		
         });
     }
 	
+	
     async run(message, args) {
+	const role = args.role;
         const member = args.member;
-    	let amount= args.days;
+    	var amount= args.days;
 	if(message.author.hasPermission("MANAGE_ROLES_OR_PERMISSIONS") && message.author.hasPermission("BAN_MEMBERS")){
-          if(member.hasRole("355056422893125632") || member.hasRole("355056425640132609") || member.hasRole("355056430266449922")){
-		return msg.reply("User already has insurance");
+          if(member.hasRole(role.id)){
+		return msg.reply("User already has ${role.name} insurance!");
 	  }
 	if(amount > 30){amount=30;}
-		
+	
+	var new_date = new Date();
+	new_date.setDate(new_date.getDate() + amount);
+	new_date.toISOString().substring(0, 10);
+
+	
+	var client = new pg.Client(connectionString);
+        client.connect();
+        console.log("Connected to Mysql");
+        client.query('INSERT INTO insurance_dates(`userid`,`roleid`,`removeDate`) VALUES($1,$2,$3)', [member.id,role.id,new_date], function (err, result) {
+            if (err) {
+                console.error(err);
+            }
+            else{
+		    console.log(result);
+		    member.addRole(role.id,"Insurance for "+amount+" days");
+		    message.reply("You've added the insurance to $1",member.username);
+		    console.log("end");
+		    client.end();
+	    }
+        });	
 	}else{ return msg.reply("You don't have the right permissions[BAN,MANAGE ROLES]");}
     }
 }
