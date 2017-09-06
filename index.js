a608b1b06987c66e3d5f497a71178ee6ff1cb441
Random2RandomRandom <whynewacc123@gmail.com>
2017-09-06 21:10:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.registry.registerCommandsIn(__dirname + "/commands") ;
 
 var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.login(process.env.BOT_TOKEN);*/
-
+/*
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
@@ -29,3 +29,17 @@ client.on('message', message => {
   }
 });
 client.login(process.env.USER_TOKEN);
+*/
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+const fs = require("fs");
+var pg = require("pg");
+bot.registry.registerGroup('random', 'Random');
+
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+var connectionString = process.env.DATABASE_URL;
+var pgClient = new pg.Client(connectionString);
+//var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+bot.login(process.env.USER_TOKEN);
