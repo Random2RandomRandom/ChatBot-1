722e707c71f11a41492bd112b367f9be54cd1730
Random2RandomRandom <whynewacc123@gmail.com>
2017-09-07 16:44:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,17 +37,16 @@ const fs = require("fs");
 var pg = require("pg");
 bot.registry.registerGroup('random', 'Random');
 
+var connectionString = process.env.DATABASE_URL;
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
-var connectionString = process.env.DATABASE_URL;
 client.on('ready', () => {
   console.log("test");
   var client = new pg.Client(process.env.DATABASE_URL);
   client.connect();
   
-  
   client.query('CREATE DATABASE IF NOT EXISTS `globaldb`');
-  client.query('CREATE TABLE IF NOT EXISTS `globaldb`.`insurance` (     
+  /*client.query('CREATE TABLE IF NOT EXISTS `globaldb`.`insurance` (     
       `userid` VARCHAR(22) NOT NULL,
       `roleid` VARCHAR(22) NOT NULL,
       `added_byid` VARCHAR(22),
@@ -55,7 +54,7 @@ client.on('ready', () => {
       `removeAtDate` date NOT NULL,
       PRIMARY KEY (`userid`,`roleid`)
     )'
-  );
+  );*/
 });
 //var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.login(process.env.USER_TOKEN);
