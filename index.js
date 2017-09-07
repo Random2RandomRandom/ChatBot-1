8adcf1d04396bc75dd6dbfebe9e816ec1beb6ea6
Random2RandomRandom <whynewacc123@gmail.com>
2017-09-07 16:45:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('ready', () => {
   client.connect();
   
   client.query('CREATE DATABASE IF NOT EXISTS `globaldb`');
-  client.query('CREATE TABLE IF NOT EXISTS `globaldb`.`insurance` (     
+  /*client.query('CREATE TABLE IF NOT EXISTS `globaldb`.`insurance` (     
       `userid` VARCHAR(22) NOT NULL,
       `roleid` VARCHAR(22) NOT NULL,
       `added_byid` VARCHAR(22),
@@ -54,7 +54,7 @@ client.on('ready', () => {
       `removeAtDate` date NOT NULL,
       PRIMARY KEY (`userid`,`roleid`)
     )'
-  );
+  );*/
 });
 //var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 bot.login(process.env.USER_TOKEN);
